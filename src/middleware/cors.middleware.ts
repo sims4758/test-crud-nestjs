4592dccdf2c4577917cb23dd502e5f35e729ee36
sims4758 <Kachanan.kcn@gmail.com>
2025-01-31 17:02:08 +0700

    update cors

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // อนุญาต Origin จาก frontend
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
